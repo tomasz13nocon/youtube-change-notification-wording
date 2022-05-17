6602898247172687e4258c8ca632b28f3a705ac3
Tomasz Nocoń <tomasz13nocon@gmail.com>
2022-05-17 13:18:18 +0200

    add multiple language handling, english regexes, light/dark mode handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Only handles video upload, livestream and premiere notifications.
 Among remaining types are comment replies notifications.
+
+Depends on the specific wording, and therefore language, of the notification text. 
+By default works with polish and english.
+You can add a language by appending to the `langs` object at the beginning of the script.
--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Change notification wording
 // @namespace    https://github.com/tomasz13nocon
-// @version      1.0
-// @description  Remove sentence-like wording from yt notifications. Dependent on the specific wording, and therefore language, of the notification text. Currently only works with polish.
+// @version      1.1
+// @description  Replace sentence-like wording from yt notifications with predictable, readable-at-a-glance text.
 // @author       Tomasz Nocoń
 // @match        https://www.youtube.com/*
 // @grant        none
@@ -13,12 +13,37 @@
 (function() {
     'use strict';
 
-    function main(list) {
-        /*let d = document.createElement("div");
-        d.style = "color:white; position:fixed; top:0; right:600px; background:#111; z-index:5000; font-size:14px;";
-        document.body.append(d);*/
+    const langs = {
+        pl: {
+            video: /Na kanał (?<channel>.*?) został przesłany film (?<title>.*)/,
+            // Youtube goes crazy when the notification text gets changed,
+            // and adds text from different notifications to existing notification elements.
+            // The span stuff works around that.
+            live: /(?:.*<\/span>)*(?<channel>.*?) nadaje: (?<title>.*)/,
+            premiere: /Na kanale (?<channel>.*?) trwa premiera filmu: (?<title>.*)/,
+        },
+        en: {
+            video: /(?<channel>.*?) uploaded: (?<title>.*)/,
+            live: /(?:.*<\/span>)*(?<channel>.*?) is live: (?<title>.*)/,
+            premiere: /(?<channel>.*?) premiering now: (?<title>.*)/,
+        },
+    };
 
+    let siteLang;
+
+    document.head.insertAdjacentHTML("beforeend",
+`<style>
+.notif-wording_channel-name {
+    color: var(--yt-spec-text-primary);
+}
+html[dark="true"] .notif-wording_channel-name {
+    color: #99a;
+}
+</style>`);
+
+    function main(list) {
         let weMutatedDom = false;
+        let styleObserverActive = false;
 
         new MutationObserver((mutations, observer) => {
             if (weMutatedDom) {
@@ -28,12 +53,39 @@
 
             let elList = list.querySelectorAll(".message.ytd-notification-renderer");
 
+            // The changes we make to notification wording mess up youtube's proccessing of them
+            // that happens when the notification drop down gets reopened, scrolled down or a new notification gets added.
+            // To remedy this we need to watch for when the drop down gets hidden and revert all our changes.
+            if (!styleObserverActive) {
+                // let listHtml = list.querySelector("#items").innerHTML;
+                let listWrapper = list.querySelector("tp-yt-iron-dropdown");
+                new MutationObserver((mutations, observer) => {
+                    console.log("style mutation");
+                    if (listWrapper.style.display === "none") {
+                        // Surprisingly  removing the HTML works.
+                        // When the drop down reopens youtube figures everything out and fills it with notifications.
+                        list.querySelector("#items").innerHTML = "";
+                        styleObserverActive = false;
+                        observer.disconnect();
+                    }
+                }).observe(listWrapper, { attributes: true, attributeFilter: ["style"] });
+                styleObserverActive = true;
+            }
+
             for (let el of elList) {
-                let style = "color: #99a;";
                 weMutatedDom = true;
-                el.innerHTML = el.innerHTML.replace(/Na kanał (.*) został przesłany film(.*)/, `<span><strong style="${style}">$1</strong>:$2</span>`);
-                el.innerHTML = el.innerHTML.replace(/(?:.*<\/span>)*(.*) nadaje:(.*)/,         `<span>🔴 <strong style="${style}">$1</strong>:$2</span>`); // The regex weirdness is a workaround for the same thing as the if statement below
-                el.innerHTML = el.innerHTML.replace(/Na kanale (.*) trwa premiera filmu:(.*)/, `<span>🎦 <strong style="${style}">$1</strong>:$2</span>`); // Alternative emojis: 🗓️📹
+
+                // Figure out the language being used
+                if (!siteLang) {
+                    for (let [langName, lang] of Object.entries(langs)) {
+                        if (Object.values(lang).some(re => re.test(el.innerHTML)))
+                            siteLang = langName;
+                    }
+                }
+
+                el.innerHTML = el.innerHTML.replace(langs[siteLang].video,    `<span><strong class="notif-wording_channel-name">$<channel></strong>: $<title></span>`);
+                el.innerHTML = el.innerHTML.replace(langs[siteLang].live,     `<span>🔴 <strong class="notif-wording_channel-name">$<channel></strong>: $<title></span>`);
+                el.innerHTML = el.innerHTML.replace(langs[siteLang].premiere, `<span>🎦 <strong class="notif-wording_channel-name">$<channel></strong>: $<title></span>`); // Alternative emojis: 🗓️📹
 
                 // Youtube goes crazy when the notification text gets changed, and adds text from different notifications to existing notification elements. This works around that.
                 if (el.children.length > 1) {
